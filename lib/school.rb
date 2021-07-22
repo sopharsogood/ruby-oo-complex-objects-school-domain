ebc75da46895a9b8df7b5c12bd19686b6207556f
sopharsogood <justanothersophie@gmail.com>
2021-07-22 13:57:18 -0400

    commit tree 0870d4473b607519addaa596145e572ea7695bf2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    attr_accessor :name
+    attr_accessor :roster
+
+    def add_student(name, gradeyear)
+        if @roster.keys.include?(gradeyear)
+            @roster[gradeyear] << name
+        else
+            @roster[gradeyear] = [name]
+        end
+    end
+
+    def grade(gradeyear)
+        @roster[gradeyear]
+    end
+
+    def sort
+        @roster.each do |gradeyear, students|
+            students.sort!
+        end
+        @roster
+    end
+end
